3ff5c652a15bc97154d807640329ff27d0bc488f
Danil <DanilBudanovEDU@yandex.ru>
2023-10-15 18:11:48 +0500

    Fixed a problem with incorrect data processing.

--- src/functions/SingleColumnTable/SCPreparationTable.sh
+++ src/functions/SingleColumnTable/SCPreparationTable.sh
@@ -3,7 +3,7 @@
 out_file=$1
 in_file=$2
 
-text="`cat ${out_file} | tr -d '\n'`"
+text="`cat ${out_file} | tr -s '\r\n' ' '`"
 echo ${text} | wc | awk '{print $2}' > ${in_file}
 echo ${text} >> ${in_file}
 
